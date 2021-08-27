92d6bb2e4de979f528ba70b2b490bf25653a7c51
Georgiy Gusev <gus3225@yandex.ru>
2021-08-27 22:11:47 +0500

    добавить cors зависимость 3

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const cors = require('cors')
 const { PORT = 3000 } = process.env
 const app = express()
 const corsOptions = {
-  "origin": "*",
+  "origin": "gusevgeorgy.students.nomoredomains.club",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
