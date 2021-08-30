e4eb342f8b811c8075585d0feeec3327400f4a38
Georgiy Gusev <gus3225@yandex.ru>
2021-08-30 22:25:27 +0500

    fix пофиксить ошибки cors

--- backend/app.js
+++ backend/app.js
@@ -35,10 +35,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 })
 
-app.use(cors({
-  origin: 'https://gusevgeorgy.students.nomoredomains.club',
-  credentials: true,
-}))
+app.use(cors())
 
 app.use(requestLogger)
 
