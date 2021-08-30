6a6bfb32fdcd1c0b5d6e90b7fad7588e29a1ea75
Georgiy Gusev <gus3225@yandex.ru>
2021-08-30 22:30:21 +0500

    fix пофиксить ошибки cors

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ app.post('/signup',
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&=]*)/.test()),
+      avatar: Joi.string().pattern(/^(http|https):\/\/[^ "]+$/),
     }),
   }), createUser)
 
